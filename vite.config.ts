03640780a7742aa5b85b5d663092bc318857f5c3
thislooksfun <tlf@thislooks.fun>
2023-01-07 13:05:37 -0600

    build: add vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { config as loadDotenv } from "dotenv";
 import solid from "solid-start/vite";
+// @ts-expect-error -- no typings yet
+import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig(() => {
   loadDotenv();
   return {
-    plugins: [solid({ ssr: true })],
+    plugins: [
+      solid({
+        ssr: true,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+        adapter: vercel({
+          // The build currently is not edge-compatible. :(
+          edge: false,
+        }),
+      }),
+    ],
     build: {
       rollupOptions: { external: ["@prisma/client"] },
     },
